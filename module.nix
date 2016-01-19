5b621fb2bd154e23f8a84e583fd28da06bfcffcf
edef <edef@edef.eu>
2016-01-19 18:56:15 +0100

    use environment.usrbinenv if available

--- module.nix
+++ module.nix
@@ -1,9 +1,13 @@
-{ config, pkgs, lib, ... }: with lib; let
+{ lib, options, ... }: with lib; let
   env-friendly = callPackage ./. {};
-in {
-  system.activationScripts.usrbinenv = mkForce ''
-    mkdir -m 0755 -p /usr/bin
-    ln -sfn ${env-friendly}/bin/env /usr/bin/.env.tmp
-    mv /usr/bin/.env.tmp /usr/bin/env # atomically replace /usr/bin/env
-  '';
-}
+in if options.environment ? usrbinenv
+  then {
+    environment.usrbinenv = "${env-friendly}/bin/env";
+  }
+  else {
+    system.activationScripts.usrbinenv = mkForce ''
+      mkdir -m 0755 -p /usr/bin
+      ln -sfn ${env-friendly}/bin/env /usr/bin/.env.tmp
+      mv /usr/bin/.env.tmp /usr/bin/env # atomically replace /usr/bin/env
+    '';
+  }
