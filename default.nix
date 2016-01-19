083f1525fc882108429a19fc3b379f6b220fc1aa
edef <edef@edef.eu>
2016-01-19 13:10:45 +0100

    use our own unpackPhase

--- default.nix
+++ default.nix
@@ -1,7 +1,12 @@
 { stdenv, musl, ... }:
 stdenv.mkDerivation {
   name = "env-1.0.0";
-  src = ./.;
+
+  unpackPhase = ''
+    cp -v ${./Makefile} Makefile
+    cp -v ${./env.c} env.c
+  '';
+
   CFLAGS = "-isystem ${musl}/include -B${musl}/lib -L${musl}/lib";
   LDFLAGS = "-static -Wl,--gc-sections";
 }
