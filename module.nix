6dda2a7c0e6c6126486142613362dc3627351ca2
edef <edef@edef.eu>
2017-04-23 16:24:57 +0200

    simplify the NixOS module

--- module.nix
+++ module.nix
@@ -1,13 +1,3 @@
-{ lib, options, ... }: with lib; let
-  env-friendly = callPackage ./. {};
-in if options.environment ? usrbinenv
-  then {
-    environment.usrbinenv = "${env-friendly}/bin/env";
-  }
-  else {
-    system.activationScripts.usrbinenv = mkForce ''
-      mkdir -m 0755 -p /usr/bin
-      ln -sfn ${env-friendly}/bin/env /usr/bin/.env.tmp
-      mv /usr/bin/.env.tmp /usr/bin/env # atomically replace /usr/bin/env
-    '';
-  }
+{ pkgs, ... }:
+let pkg = callPackage ./. {}; in
+{ environment.usrbinenv = "${env-friendly}/bin/env"; }
